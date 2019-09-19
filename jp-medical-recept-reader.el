f9b6fde3a075532836e578187c27dc8634fc7876
Taiju Aoki <aokitaiju0513@gmail.com>
2019-09-19 20:54:04 +0900

    update

--- jp-medical-recept-reader.el
+++ jp-medical-recept-reader.el
@@ -1,8 +1,44 @@
-(defvar recept-posframe-buffer " *recept-posframe")
-(defvar recept-buffer-showing nil)
+;;; jp-medical-recept-reader --- show japanese medical recept info
+
+;; Author: blue0513
+;; URL: https://github.com/blue0513/point-history
+;; Version: 0.1.0
+
+;;; Commentary:
+
+;; Edit your init.el
+;;
+;; (require 'jp-medical-recept-reader)
+;;
+
+;;; Code:
+
+(require 'posframe)
+(require 'dash)
+
+(defvar mrr-recept-posframe-buffer " *recept-posframe")
+(defvar mrr-recept-buffer-showing nil)
+
+(defun mrr--build-day-info (limit)
+  (let* ((value))
+    (dotimes (number limit value)
+      (let* ((str (format "%s日の情報" (+ 1 number))))
+        (setq value (cons str value))))
+    (reverse value)))
 
 (defvar ir-record-header
-  (list))
+  (list
+   "レコード識別情報"
+   "審査支払機関"
+   "都道府県"
+   "点数表"
+   "医療機関コード"
+   "予備"
+   "医療機関名称"
+   "請求年月"
+   "マルチボリューム識別情報"
+   "電話番号"
+   ))
 
 (defvar re-record-header
   (list
@@ -45,9 +81,6 @@
    "カタカナ（氏名）"
    "患者の状態"))
 
-(defvar gr-record-header
-  (list))
-
 (defvar ho-record-header
   (list
    "レコード識別情報"
@@ -81,6 +114,12 @@
    "食事療養・生活療養回数"
    "食事療養・生活療養合計金額"))
 
+(defvar gr-record-header
+  (list
+   "レコード識別情報"
+   "医科点数表算定理由"
+   "ＤＰＣコード"))
+
 (defvar sy-record-header
   (list
    "レコード識別情報"
@@ -93,67 +132,135 @@
    "補足コメント"))
 
 (defvar si-record-header
-  (list))
+  (-flatten (list
+   "レコード識別情報"
+   "診療識別"
+   "負担区分"
+   "診療行為コード"
+   "数量データ"
+   "点数"
+   "回数"
+   "コメントコード"
+   "文字データ"
+   "コメントコード"
+   "文字データ"
+   "コメントコード"
+   "文字データ"
+   (mrr--build-day-info 31))))
+
+(defvar iy-record-header
+  (-flatten (list
+   "レコード識別情報"
+   "診療識別"
+   "負担区分"
+   "医薬品コード"
+   "使用量"
+   "点数"
+   "回数"
+   "コメントコード"
+   "文字データ"
+   "コメントコード"
+   "文字データ"
+   "コメントコード"
+   "文字データ"
+   (mrr--build-day-info 31))))
 
 (defvar to-record-header
-  (list))
+  (-flatten (list
+   "レコード識別情報"
+   "診療識別"
+   "負担区分"
+   "特定器材コード"
+   "使用量"
+   "点数"
+   "回数"
+   "単位コード"
+   "単価"
+   "予備"
+   "商品名及び規格又はサイズ"
+   "コメントコード"
+   "文字データ"
+   "コメントコード"
+   "文字データ"
+   "コメントコード"
+   "文字データ"
+   (mrr--build-day-info 31))))
 
 (defvar co-record-header
-  (list))
+  (list
+   "レコード識別情報"
+   "診療識別"
+   "負担区分"
+   "コメントコード"
+   "文字データ"))
 
 (defvar sj-record-header
-  (list))
+  (list
+   "レコード識別情報"
+   "症状詳記区分"
+   "症状詳記データ"))
 
-(defun build-message (result)
+(defun mrr--build-message (result)
   (let* ((value ""))
     (dolist (element result)
       (setq value
             (concat value (format "%s : %s\n" (car element) (cdr element)))))
     value))
 
-(defun show-recept-info-of (record-header my-list)
+(defun mrr-show-recept-info-of (record-header my-list)
   (let* ((result (mapcar* #'cons record-header my-list))
-         (str (build-message result)))
-    (recept-show str)))
+         (str (mrr--build-message result)))
+    (mrr-recept-show str)))
 
-(defun recept-show (str)
+(defun mrr-recept-show (str)
   (posframe-show
-   recept-posframe-buffer
+   mrr-recept-posframe-buffer
    :string str
+   :posion (point)
+   :min-width 40
    :no-properties nil
+   :internal-border-width 10
    :background-color "black")
-  (setq recept-buffer-showing t))
+  (setq mrr-recept-buffer-showing t))
 
-(defun show-recept-info ()
+(defun mrr-show-recept-info ()
   (let* ((line (thing-at-point 'line t))
          (my-list (split-string line ",")))
     (when (string= (car my-list) "IR")
-      (show-recept-info-of ir-record-header my-list))
+      (mrr-show-recept-info-of ir-record-header my-list))
     (when (string= (car my-list) "RE")
-      (show-recept-info-of re-record-header my-list))
-    (when (string= (car my-list) "GR")
-      (show-recept-info-of gr-record-header my-list))
+      (mrr-show-recept-info-of re-record-header my-list))
     (when (string= (car my-list) "HO")
-      (show-recept-info-of ho-record-header my-list))
+      (mrr-show-recept-info-of ho-record-header my-list))
     (when (string= (car my-list) "KO")
-      (show-recept-info-of ko-record-header my-list))
+      (mrr-show-recept-info-of ko-record-header my-list))
+    (when (string= (car my-list) "GR")
+      (mrr-show-recept-info-of gr-record-header my-list))
     (when (string= (car my-list) "SY")
-      (show-recept-info-of sy-record-header my-list))
+      (mrr-show-recept-info-of sy-record-header my-list))
     (when (string= (car my-list) "SI")
-      (show-recept-info-of si-record-header my-list))
+      (mrr-show-recept-info-of si-record-header my-list))
+    (when (string= (car my-list) "IY")
+      (mrr-show-recept-info-of iy-record-header my-list))
     (when (string= (car my-list) "TO")
-      (show-recept-info-of to-record-header my-list))
+      (mrr-show-recept-info-of to-record-header my-list))
     (when (string= (car my-list) "CO")
-      (show-recept-info-of co-record-header my-list))
+      (mrr-show-recept-info-of co-record-header my-list))
     (when (string= (car my-list) "SJ")
-      (show-recept-info-of sj-record-header my-list))))
+      (mrr-show-recept-info-of sj-record-header my-list))))
 
-(defun hide-recept-info ()
-  (setq recept-buffer-showing nil)
-  (posframe-delete recept-posframe-buffer))
+(defun mrr-hide-recept-info ()
+  (setq mrr-recept-buffer-showing nil)
+  (posframe-delete mrr-recept-posframe-buffer))
 
-(defun toggle-recept-info ()
+(defun recept-info-toggle ()
   (interactive)
-  (if recept-buffer-showing
-      (hide-recept-info)
-    (show-recept-info)))
+  (if mrr-recept-buffer-showing
+      (mrr-hide-recept-info)
+    (mrr-show-recept-info)))
+
+;; * provide
+
+(provide 'jp-medical-recept-reader)
+
+;;; jp-medical-recept-reader.el ends here
